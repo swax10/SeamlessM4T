d7c1be06d4e383064188336978af6322d601a850
Abhi-vish <iamabhishekvishwakarma7@gmail.com>
2023-09-08 17:51:08 +0530

    Update Commit

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
  
-@app.route('/')
-def hello_world():
-    return render_template('home.html')
- 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        selected_language = request.form.get('disabledSelect')
+        audio_file = request.files.get('fileInput')
+
+        if audio_file and selected_language:
+            translated_text = translate_audio(audio_file, selected_language)
+            return render_template('home.html', translation=translated_text)
+
+    return render_template('home.html', translation=None)
+
+def translate_audio(audio_file, target_language):
+    translated_text = f'Translated text in {target_language}'
+    return translated_text
+
 if __name__ == '__main__':
     app.run(debug=True)
